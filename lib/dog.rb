e8e60200e2ff3ca849da17048c4424361be8caab
Charles Pelton <charliejp0311@gmail.com>
2020-02-04 15:51:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
   attr_accessor :name
   def initialize(name)
     @name = name
-    self.save 
+    self.save
   end
   def self.all
     @@all
@@ -19,7 +19,7 @@ class Dog
   def self.clear_all
     @@all.clear
   end
-  def self.save 
+  def self.save
     @@all << self
   end
 end
